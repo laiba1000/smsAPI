1b550240a511033771e36a593a9cd5cada622dab
laiba1000 <bscs2312374@szabist.pk>
2025-05-06 10:28:45 +0000

    Create get task

--- studentAPI/index.js
+++ studentAPI/index.js
@@ -32,6 +32,21 @@ app.get('gettotalstd',async(reg,res)=>{
     }
 });
 
+app.get('/employeesintoronto', async (req, res) => {
+    try {
+        const result = await pool.query(`
+            SELECT e.employee_id, e.first_name, e.last_name, j.job_title
+            FROM employees e
+            JOIN jobs j ON e.job_id = j.job_id
+            JOIN departments d ON e.department_id = d.department_id
+            JOIN locations l ON d.location_id = l.location_id
+            WHERE l.city = 'Toronto'
+        `);
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ Error: err.message });
+    }
+});
 
 
 
